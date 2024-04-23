d66cb357461f8cfef34569e281f224649a417be6
Bui Tuan <147406431+buituandev@users.noreply.github.com>
2024-04-23 14:20:37 +0700

    first commit
    ver 0.1

---
+++ Tenant.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author buitu
+ */
+public class Tenant {
+    private User user;
+    private RentalContract rentalContract;
+
+    public Tenant() {
+    }
+
+    public Tenant(User user, RentalContract rentalContract) {
+        this.user = user;
+        this.rentalContract = rentalContract;
+    }
+
+    public boolean requestCreateRentalContract(String contractId, String propertyId, String userId, float rentAmount) {
+        if (rentalContract == null) {
+            rentalContract = new RentalContract(contractId, propertyId, userId, rentAmount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean requestTerminateRentalContract() {
+        if (rentalContract != null) {
+            rentalContract = null;
+            return true;
+        }
+        return false;
+    }
+}
