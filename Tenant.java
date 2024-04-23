555422fc912769701ce91b842a3bb7b551430764
Bui Tuan <147406431+buituandev@users.noreply.github.com>
2024-04-23 14:25:30 +0700

    Update Tenant.java
    ver 0.2

--- Tenant.java
+++ Tenant.java
@@ -3,10 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author buitu
- */
 public class Tenant {
     private User user;
     private RentalContract rentalContract;
